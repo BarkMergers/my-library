84444eb152588d3c361127a25513079bc64bac61
Mark Burgess <mburgess1983@gmail.com>
2025-09-29 14:31:44 +0100

    v1.6.5

--- my-library/vite.config.ts
+++ my-library/vite.config.ts
@@ -1,30 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+// import tailwindcss from '@tailwindcss/vite';tailwindcss(),
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss(),
-        dts()
-    ],
+    plugins: [react(), dts()],
     build: {
         lib: {
             entry: "./src/index.ts",
             name: "MyLibrary",
-            formats: ["es", "umd"],
+            formats: ["es"],
             fileName: (format) => `my-library.${format}.js`,
         },
         rollupOptions: {
             external: ["react", "react-dom"],
-            output: {
-                globals: {
-                    react: "React",
-                    "react-dom": "ReactDOM"
-                }
-            }
-        }
-    }
+        },
+    },
 });
